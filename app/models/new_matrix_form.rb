27512b20ca4d3a38b24229f2b30593d13afb1909
miguel jimenez <denkatalabeno@gmail.com>
2020-11-27 03:02:54 +0800

    Matrix Form initializes with 4 exercise slots by default, extensible to more or less exercises

--- app/models/new_matrix_form.rb
+++ app/models/new_matrix_form.rb
@@ -5,15 +5,16 @@ class NewMatrixForm
   validate :slots_cannot_have_the_same_exercise
   validate :competencies_cannot_be_repeated_in_an_exercise
 
-  attr_accessor :exercise_slot_1
-  attr_accessor :exercise_slot_2
-  attr_accessor :exercise_slot_3
-  attr_accessor :exercise_slot_4
+  # attr_accessor :exercise_slot_1
+  # attr_accessor :exercise_slot_2
+  # attr_accessor :exercise_slot_3
+  # attr_accessor :exercise_slot_4
 
-  attr_accessor :competency_slot_1
-  attr_accessor :competency_slot_2
-  attr_accessor :competency_slot_3
-  attr_accessor :competency_slot_4
+  def initialize(ex_number=4)
+    ex_number.times do |n|
+      singleton_class.class_eval { attr_accessor "exercise_slot_#{n + 1}" }
+    end
+  end
 
   def slots_cannot_have_the_same_exercise
     slots = [exercise_slot_1, exercise_slot_2, exercise_slot_3, exercise_slot_4]
