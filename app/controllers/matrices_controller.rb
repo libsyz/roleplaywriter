23ef7a379139fe4402fdea8e35118ff491cc05b9
miguel jimenez <denkatalabeno@gmail.com>
2020-11-28 00:07:29 +0800

    reorganized the code in the matrix form class, ready to start working on the matrix creation flow

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -1,16 +1,16 @@
 class MatricesController < ApplicationController
   def new
     @center = Center.find(params[:center_id])
-    @new_matrix_form = NewMatrixForm.new
     @exercises = @center.storyline.exercises
     @competencies = @center.competency_model.competencies
+    @matrix = NewMatrixForm.new
   end
 
   def create
-    @new_matrix_form = NewMatrixForm.new(matrix_form_params)
-
+    @new_matrix_form = NewMatrixForm.new
+    @new_matrix_form.build_from(matrix_form_params)
     if @new_matrix_form.save
-      redirect_to some_path
+      redirect_to root_path
     else
       render :new
     end
@@ -18,6 +18,6 @@ class MatricesController < ApplicationController
 
   def matrix_form_params
     binding.pry
-    params.require
+    params.require(:matrix)
   end
 end
--- app/models/new_matrix_form.rb
+++ app/models/new_matrix_form.rb
@@ -8,32 +8,15 @@ class NewMatrixForm
   def initialize(ex_number=4, comps_per_exercise=4)
     @ex_slots = ex_number
     @comps_per_exercise = comps_per_exercise
-    generate_slots_and_competencies
+    create_ex_slots_and_competencies
     super()
   end
 
-  def slots_cannot_have_the_same_exercise
-    errors.add(:ex_slots, "exercises cannot be repeated" ) if ex_slot_values != ex_slot_values.uniq
-    errors.add(:ex_slots, "you must select at least one exercise" ) if ex_slot_values.empty?
+  def build(form_params)
+    # add the right form values to the right ex_slots and competencies.
+    # this method couples the view to the form
   end
 
-  def competencies_cannot_be_repeated_in_an_exercise
-    # loop through the number of exercise names
-    @ex_slots.times do |n|
-      arr = []
-      @comps_per_exercise.times do |i|
-        arr << instance_variable_get("@ex_slot_#{n + 1}_comp_#{i + 1}")
-      end
-
-      case
-      when arr.compact.empty?
-        errors.add("ex_slot_#{n + 1}".to_sym, "You must select at least one competency for this exercise")
-      when arr != arr.uniq && arr.length > 1
-        errors.add("ex_slot_#{n + 1}".to_sym, "competencies cannot be repeated in the same exercise")
-      end
-
-    end
-  end
 
   def save
     return false if invalid?
@@ -41,9 +24,14 @@ class NewMatrixForm
     Matrix.new()
   end
 
+  def slots_cannot_have_the_same_exercise
+    errors.add(:ex_slots, "exercises cannot be repeated" ) if ex_slot_values != ex_slot_values.uniq
+    errors.add(:ex_slots, "you must select at least one exercise" ) if ex_slot_values.empty?
+  end
+
   private
 
-  def generate_slots_and_competencies
+  def create_ex_slots_and_competencies
     @ex_slots.times do |n|
       singleton_class.class_eval { attr_accessor "exercise_slot_#{n + 1}" }
       @comps_per_exercise.times do |i|
@@ -55,6 +43,23 @@ class NewMatrixForm
   def ex_slot_values
     instance_variables.select { |attr| attr.match?(/@exercise_slot_/ )}
                                        .map { |var| self.instance_variable_get(var) }
+  end
+
+
+  def competencies_cannot_be_repeated_in_an_exercise
+    @ex_slots.times do |n|
+      arr = []
+      @comps_per_exercise.times do |i|
+        comps_in_exercise << instance_variable_get("@ex_slot_#{n + 1}_comp_#{i + 1}")
+      end
+
+      case
+      when comps_in_exercise.compact.empty?
+        errors.add("ex_slot_#{n + 1}".to_sym, "You must select at least one competency for this exercise")
+      when comps_in_exercise != comps_in_exercise.uniq && comps_in_exercise.length > 1
+        errors.add("ex_slot_#{n + 1}".to_sym, "competencies cannot be repeated in the same exercise")
+      end
+    end
   end
 end
 
