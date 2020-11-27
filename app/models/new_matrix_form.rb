a863e06c1792fd217786aa771edde3dee577b66d
miguel jimenez <denkatalabeno@gmail.com>
2020-11-27 09:18:23 +0800

    refactored validation for exercise slots

--- app/models/new_matrix_form.rb
+++ app/models/new_matrix_form.rb
@@ -6,30 +6,33 @@ class NewMatrixForm
   #validate :competencies_cannot_be_repeated_in_an_exercise
 
   def initialize(ex_number=4)
-    @ex_number = ex_number
-    @ex_number.times do |n|
-      singleton_class.class_eval { attr_accessor "exercise_slot_#{n + 1}" }
-    end
+    @ex_slots = ex_number
+    generate_slots
     super()
   end
 
   def slots_cannot_have_the_same_exercise
-    # TODO: This does not work anymore, exercise slots are dynamically generated
-    # get all the values that belong to instance props exercise_slot_n
-    # throw them into an array
-    # compare them
-    # before I do this, I need to decide how do I want to model the data in the
-    # exercises and the competencies
-
-    ex_slot_values = instance_variables.select { |attr| attr.match?(/@exercise_slot_/ )}
-                                       .map { |var| self.instance_variable_get(var) }
-
     errors.add(:ex_slots, "exercises cannot be repeated" ) if ex_slot_values != ex_slot_values.uniq
     errors.add(:ex_slots, "you must select at least one exercise" ) if ex_slot_values.empty?
   end
 
   def save
+    return false if invalid?
+
     Matrix.new()
+  end
+
+  private
+
+  def generate_slots
+    @ex_slots.times do |n|
+      singleton_class.class_eval { attr_accessor "exercise_slot_#{n + 1}" }
+    end
+  end
+
+  def ex_slot_values
+    instance_variables.select { |attr| attr.match?(/@exercise_slot_/ )}
+                                       .map { |var| self.instance_variable_get(var) }
   end
 
 end
